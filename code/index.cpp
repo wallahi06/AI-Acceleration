58a9e9139940b85484e3356246785622522d5b1e
Liam Nordvall <86798194+wallahi06@users.noreply.github.com>
2022-07-03 22:32:45 +0200

    Update index.cpp

--- code/index.cpp
+++ code/index.cpp
@@ -1,133 +1,130 @@
-#include <iostream>
-#include <vector>
-#include <cstdlib>
+#include <iostream> 
+#include <vector> 
 
+
+void error_handler(int error_code);
 
 class Vector {
     public:
-        Vector(unsigned int itsCol, unsigned int itsRow);
+        Vector(unsigned int itsRow, unsigned int itsCol);
         ~Vector() {};
         
-        unsigned int input_size;
-        std::vector<int> input_matrix;
-        
+        std::vector<int> input_vector;
+
         void show();
         
-        // Functions for cretaing different types of matrixes
         std::vector<int> zeros();
         std::vector<int> ones();
-        std::vector<int> random(unsigned int size);
-
-        // Functions for calculations
-        std::vector<int> sum(std::vector<int> term_vector);
-        std::vector<int> diff(std::vector<int> term_vector);
-        std::vector<int> product(std::vector<int> term_vector);
-
-    private:
-        unsigned int num_col;
-        unsigned int num_row;
-
+        std::vector<int> random(unsigned int i);
         
-};
-
-Vector::Vector(unsigned int itsCol, unsigned int itsRow) {
-    num_col = itsCol;
-    num_row = itsRow;
-    input_size = num_col * num_row;
-    input_matrix.resize(input_size);
-
-}
-
-// members of the Vector class that takes care of creating and formatting the matrices
-std::vector<int> Vector::zeros() {
-    for (unsigned int i = 0; i < input_size; i++) {
-        input_matrix[i] = 0;
-    }
-    return input_matrix;
-}
-
-std::vector<int> Vector::ones() {
-    for (unsigned int i = 0; i < input_size; i++) {
-        input_matrix[i] = 1;
-    }
-    return input_matrix;
-}
-
-std::vector<int> Vector::random(unsigned int size = 100) {
-    for (unsigned int i = 0; i < input_size; i++) {
-        input_matrix[i] = rand() % size;
-    }
+        Vector sum(Vector term_vector);
+        Vector diff(Vector term_vector);
     
-    return input_matrix;
+        
+    private:
+        unsigned int Row_size;
+        unsigned int Col_size;
+        unsigned int input_size;
+}; 
+
+
+// constructor of the Vector class
+Vector::Vector(unsigned int itsRow, unsigned int itsCol) {
+    
+    Row_size = itsRow;
+    Col_size = itsCol;
+    
+    input_size = itsRow * itsCol;
+    input_vector.resize(input_size);
+    
 }
 
 
-// Outputs the values oriented in the correct order for visulizing the matrices
 void Vector::show() {
     for (unsigned int i = 0; i < input_size; i++) {
-        if (i % num_col == 0 && i != 0) {
-            std::cout << std::endl;
+        if (i % Col_size == 0 && i != 0) {
+            std::cout << std::endl << std::endl;
         }
-        std::cout << input_matrix[i] << "  ";
-        
+        std::cout << input_vector[i] << "  ";
     }
+    std::cout << std::endl << std::endl << std::endl;
 }
 
-// members of the Vector class that takes care of the calculations
-// returns the sum of vector1 and vector2
-std::vector<int> Vector::sum(std::vector<int> term_vector) {
-    
-    Vector output_matrix(2, 2);
-    output_matrix.zeros();
-    
+
+// member of the Vector class that returns a vector of zeros
+std::vector<int> Vector::zeros() {
     for (unsigned int i = 0; i < input_size; i++) {
-        output_matrix.input_matrix[i] = input_matrix[i] + term_vector[i];
+        input_vector[i] = 0;
     }
-    return output_matrix.input_matrix;
+    
+    return input_vector;
 }
 
-// returns the difference between vector1 and vector2
-std::vector<int> Vector::diff(std::vector<int> term_vector) {
-    
-    Vector output_matrix(2, 2);
-    output_matrix.zeros();
-    
+// member of the Vector class that returns a vector of ones
+std::vector<int> Vector::ones() {
     for (unsigned int i = 0; i < input_size; i++) {
-        output_matrix.input_matrix[i] = input_matrix[i] - term_vector[i];
+        input_vector[i] = 1; 
     }
-    return output_matrix.input_matrix;
+    
+    return input_vector;
 }
 
-// returns the product of vector1 and vector2
-std::vector<int> Vector::product(std::vector<int> term_vector) {
-    Vector output_matrix(2, 2);
-    output_matrix.zeros();
+//member of the Vector class that returns a vector of random values
+std::vector<int> Vector::random(unsigned int size = 100) {
+    for (unsigned int i = 0; i < input_size; i++) {
+        input_vector[i] = rand() % size;
+    }
     
-    for (unsigned int i = 0; i < 2; i++) {    
-        for (unsigned int j = 0; j < 2; j++) {
-            for (unsigned int k = 0; k < 2; k++) {
-                output_matrix.input_matrix[i * 2 + j] += input_matrix[i * 2 + k] * term_vector[k * 2 + j];
-            }
+    return input_vector;
+}
+
+
+
+// member of the Vector class that adds vector1 with vector2
+Vector Vector::sum(Vector term_vector) {
+    
+    Vector output_vector(Row_size, Col_size);
+    output_vector.zeros();
+    
+    try {
+        if (Col_size == term_vector.Col_size && Row_size == term_vector.Row_size) {
+            for (unsigned int i = 0; i < input_size; i++) {
+                output_vector.input_vector[i] = input_vector[i] + term_vector.input_vector[i];
+                }
+        } else {
+            throw std::runtime_error("invalid size, cant combine different dimensions");
         }
+    } catch (std::runtime_error &error) {
+        std::cout << "Error occured: " << error.what() << std::endl;
     }
     
-    return output_matrix.input_matrix;
+    return output_vector;
 }
 
+
+// member of the Vector class that subtracts vector1 with vector2
+Vector Vector::diff(Vector term_vector) {
+    
+    Vector output_vector(Row_size, Col_size);
+    output_vector.zeros();
+
+    try {
+        if (Col_size == term_vector.Col_size && Row_size == term_vector.Row_size) {
+            for (unsigned int i = 0; i < input_size; i++) {
+                output_vector.input_vector[i] = input_vector[i] - term_vector.input_vector[i];
+                }
+        } else {
+            throw std::runtime_error("invalid size, cant combine different dimensions");
+        }
+    } catch (std::runtime_error &error) {
+        std::cout << "Error occured: " << error.what() << std::endl;
+    }
+    
+    return output_vector;
+}
 
 
 int main() {
-    
-    Vector vector1(2, 2);
-    Vector vector2(2, 2);
-    Vector vector3(2, 2);
-    
-    vector1.ones();
-    vector2.ones();
-    
-    
-    vector3.input_matrix = vector1.product(vector2.input_matrix);
-    vector3.show();
-    
     return 0;
 }
+
