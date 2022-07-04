9c93ba8ae0bae5d326bdd9b229b3ab9500205c18
Liam Nordvall <86798194+wallahi06@users.noreply.github.com>
2022-07-04 20:10:36 +0200

    Update tipo_vector.h

--- code/tipo_vector.h
+++ code/tipo_vector.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <vector>
 
@@ -5,25 +6,32 @@
 class Vector {
 public:
     // Constructor and destructor definition
-    Vector(unsigned int itsRow, unsigned int itsCol);
+    Vector(int itsRow, int itsCol);
     ~Vector() {};
 
     std::vector<int> input_vector;
 
+    // methods for getting information about your vectors
     void show();
     void dim();
 
+    // methods for creating and structuring vectors
     std::vector<int> zeros();
     std::vector<int> ones();
-    std::vector<int> random(unsigned int size);
+    std::vector<int> random(int size = 100);
+    std::vector<int> fill(std::vector<int> set_vector);
 
+    // methods for calculations of the vectors
     Vector sum(Vector term_vector);
     Vector diff(Vector term_vector);
     Vector product(Vector term_vector);
+    Vector divide(signed int denominator);
+    Vector pow(signed int exponent);
 
 private:
-    unsigned int Row_size;
-    unsigned int Col_size;
-    unsigned int input_size;
+    int row_size;
+    int col_size;
+    int input_size;
 };
+
 
