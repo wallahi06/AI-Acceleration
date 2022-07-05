4a6bba10e95a7f04ec01e002689271d41faf1b36
Liam Nordvall <86798194+wallahi06@users.noreply.github.com>
2022-07-05 21:48:45 +0200

    Update tipo_vector.h

--- code/tipo_vector.h
+++ code/tipo_vector.h
@@ -1,26 +1,28 @@
 #pragma once
-
 #include <iostream>
 #include <vector>
 
 
+// The Vector class takes care of everything related to Tipo's vector/matrix operations, such as multiplicaiton 
+// of vectors and easily creating them using one of our pre-made methods for filling your vector with random 
+// values, zeros, ones or your own values using .fill
 class Vector {
 public:
     // Constructor and destructor definition
-    Vector(int itsRow, int itsCol);
+    Vector(unsigned int itsRowSize, unsigned int itsColSize);
     ~Vector() {};
 
-    std::vector<int> input_vector;
+    std::vector<double> input_vector;
 
     // methods for getting information about your vectors
     void show();
     void dim();
 
     // methods for creating and structuring vectors
-    std::vector<int> zeros();
-    std::vector<int> ones();
-    std::vector<int> random(int size = 100);
-    std::vector<int> fill(std::vector<int> set_vector);
+    std::vector<double> zeros();
+    std::vector<double> ones();
+    std::vector<double> random(signed int interval = 100);
+    std::vector<double> fill(std::vector<double> set_vector);
 
     // methods for calculations of the vectors
     Vector sum(Vector term_vector);
@@ -30,9 +32,9 @@ public:
     Vector pow(signed int exponent);
 
 private:
-    int row_size;
-    int col_size;
-    int input_size;
+    unsigned int row_size;
+    unsigned int col_size;
+    unsigned int input_size;
 };
 
 
